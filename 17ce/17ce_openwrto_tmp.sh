41000d3b8ea677c0538ba5ebddf09fbddac098c4
xch <@>
2018-09-11 15:43:43 +0800

    del

--- 17ce/17ce_openwrto_tmp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm -f /etc/17ce/17ce_openwrt_ins.sh
-wget --no-check-certificate -O /etc/17ce/17ce_openwrt_ins.sh https://raw.githubusercontent.com/erebusx/r-app/17ce/17ce/17ce_openwrto.sh 2>/dev/null  >/dev/null
-chmod +x /etc/17ce/17ce_openwrt_ins.sh
-rm -f /etc/17ce/17ce_openwrt_tmp.sh
-
--- 17ce/17ce_padavano_tmp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -f /etc/storage/17ce/17ce_padavan_ins.sh
-wget --no-check-certificate -O /etc/storage/17ce/17ce_padavan_ins.sh https://raw.githubusercontent.com/erebusx/r-app/17ce/17ce/17ce_padavano.sh 2>/dev/null  >/dev/null
-chmod +x /etc/storage/17ce/17ce_padavan_ins.sh
-rm -f /etc/storage/17ce/17ce_padavan_tmp.sh
