265a1291937d2de1c5fb2232072432bb32171ca1
xch <@>
2018-09-12 10:33:20 +0800

    major change

--- 17ce/17ce_openwrt.sh
+++ 17ce/17ce_openwrt.sh
@@ -3,11 +3,12 @@
 
 CDN_BASE="https://raw.githubusercontent.com/erebusx/r-app/17ce/17ce"
 # http://www.17ce.com/api/17ce_route_client_new.php
-UPDATE_URL="https://raw.githubusercontent.com/erebusx/r-app/17ce/17ce/17ce_version.php"
-TEMP_FILE="/tmp/update.txt"
-UPDATE_FILE="/tmp/update.tgz"
+UPDATE_URL="$CDN_BASE/17ce_version.php"
+#TEMP_FILE="/tmp/update.txt"
+#UPDATE_FILE="/tmp/update.tgz"
 WORK_DIR="/tmp/17ce"
 SAVE_DIR="/etc/17ce"
+USERNAME="$1"
 export LD_LIBRARY_PATH=/lib:$WORK_DIR
 logging()
 {
@@ -18,100 +19,87 @@ wait_for_network(){
   sleep 2
   echo "waiting for network......"
 }
-check_update()
-{
-  wget -T 30 $UPDATE_URL -O  $TEMP_FILE
-  TURL=`cat $TEMP_FILE|awk '{print $2}'`
-  echo "will download update file -> $TURL"
-  wget -T 60 $TURL  -O $UPDATE_FILE
-}
 wget_install()
 {
-  wget -T 60 --no-check-certificate -O $1  $2 
+  wget -T 60 --no-check-certificate -O $1 $2 
   chmod +x $1
 }
-init_conf()
-{
-  #echo -e "#!/bin/sh /etc/rc.common \n# Copyright (C) 17ce.com \nstart=99 \n/etc/17ce/17ce_openwrt_ins.sh $1 2>/dev/null  >/dev/null" >/etc/init.d/17ce_openwrt
-  cat <<EOF > /etc/init.d/17ce_openwrt
-#!/bin/sh /etc/rc.common
-# Copyright (C) 17ce.com
-START=99
-STOP=10
 
-start()
+check_update()
 {
-    /etc/17ce/17ce_openwrt.sh $1
-}
-
-stop()
-{
-    killall -9 17ce_v3
-    sleep 1
-    echo "17ce Client has stoped."
-}
-EOF
-  chmod +x /etc/init.d/17ce_openwrt
-
-  if [ ! -f "/etc/rc.d/S9917ce_openwrt" ]; then
-    ln -s /etc/init.d/17ce_openwrt /etc/rc.d/S9917ce_openwrt
-    ln -s /etc/init.d/17ce_openwrt /etc/rc.d/K1017ce_openwrt
-  fi
-  if [ ! -f "/etc/crontabs/root" ]; then
-    touch /etc/crontabs/root
-    chmod +x /etc/crontabs/root
-  fi
-  sed -i /17ce_openwrt_ins.sh/d /etc/crontabs/root
-  if grep -wq "17ce_openwrt.sh" /etc/crontabs/root; then
-    echo "OK"
+  UPFLAG=1
+  MAINEXEC="$WORK_DIR/17ce_v3"
+  if [ ! -f "$MAINEXEC" ]; then
+    echo "$MAINEXEC not found"
   else
-    echo "0 */1 * * * /etc/17ce/17ce_openwrt.sh $1 &">>/etc/crontabs/root
+    VERSION=`$MAINEXEC -v`
+    TEMPSTR=`curl -k -m 10 $UPDATE_URL 2>/dev/null  || wget -q -O - $UPDATE_URL 2>/dev/null`
+    TVER=`echo $TEMPSTR|awk '{print $1}'`
+    #TURL=`echo $TEMPSTR|awk '{print $2}'`
+    echo "current version:$VERSION, newest:$TVER"
+    if [ "$VERSION" == "$TVER" ]; then
+      UPFLAG=0
+    fi
+  fi
+  
+  if [ "$UPFLAG" != "0" ]; then
+    echo "downloading files"
+    killall -9 17ce_v3 2>/dev/null  >/dev/null
+    rm -rf $WORK_DIR
+    mkdir -p $WORK_DIR
+    cd $WORK_DIR
+    #wget_install 17ce_v3      $CDN_BASE/bin/17ce_v3
+    #wget_install conf.json    $CDN_BASE/bin/conf.json
+    ##wget_install ld-uClibc.so.1   $CDN_BASE/lib/ld-uClibc.so.1
+    #wget_install libc.so      $CDN_BASE/lib/libc.so
+    #wget_install libcurl.so.4   $CDN_BASE/lib/libcurl.so.4
+    #wget_install libstdc++.so.6   $CDN_BASE/lib/libstdc
+    ##wget_install libpolarssl.so.7    $CDN_BASE/lib/libpolarssl.so.7
+    ##wget_install libpthread.so.0    $CDN_BASE/lib/libpthread.so.0
+    wget_install bin.tar.gz  $CDN_BASE/bin.tar.gz
+    wget_install lib.tar.gz  $CDN_BASE/lib.tar.gz
+    tar zxvf bin.tar.gz
+    tar zxvf lib.tar.gz
+    rm -f bin.tar.gz
+    rm -f lib.tar.gz
+  fi
+  
+  echo "update myself"
+  wget --no-check-certificate -O /tmp/17ce_openwrt.sh $CDN_BASE/17ce_openwrt.sh 2>/dev/null >/dev/null
+  if [ -f "/tmp/17ce_openwrt.sh" ]; then
+    sed -i s%WORK_DIR=.*%WORK_DIR=\"$WORK_DIR\"% /tmp/17ce_openwrt.sh
+    sed -i s%SAVE_DIR=.*%SAVE_DIR=\"$SAVE_DIR\"% /tmp/17ce_openwrt.sh
+    mkdir -p $SAVE_DIR
+    mv -f /tmp/17ce_openwrt.sh $SAVE_DIR/17ce_openwrt.sh
+    chmod +x $SAVE_DIR/17ce_openwrt.sh
   fi
 }
-init_files()
-{
-#       check_update
-  killall -9 17ce_v3 2>/dev/null  >/dev/null
-  rm -rf $WORK_DIR
-  mkdir -p $WORK_DIR
-  cd $WORK_DIR
-  #wget_install 17ce_v3      $CDN_BASE/bin/17ce_v3
-  #wget_install conf.json    $CDN_BASE/bin/conf.json
-  ##wget_install ld-uClibc.so.1   $CDN_BASE/lib/ld-uClibc.so.1
-  #wget_install libc.so      $CDN_BASE/lib/libc.so
-  #wget_install libcurl.so.4   $CDN_BASE/lib/libcurl.so.4
-  #wget_install libstdc++.so.6   $CDN_BASE/lib/libstdc
-  ##wget_install libpolarssl.so.7    $CDN_BASE/lib/libpolarssl.so.7
-  ##wget_install libpthread.so.0    $CDN_BASE/lib/libpthread.so.0
-  wget_install bin.tar.gz  $CDN_BASE/bin.tar.gz
-  wget_install lib.tar.gz  $CDN_BASE/lib.tar.gz
-  tar zxvf bin.tar.gz
-  tar zxvf lib.tar.gz
-  rm -f bin.tar.gz
-  rm -f lib.tar.gz
-}
 start()
-{        
-  if ps|grep -w "17ce_v3"|grep -v grep 2>/dev/null  >/dev/null; then
+{
+  wait_for_network
+  check_update      
+  if ps|grep -w "17ce_v3"|grep -v grep 2>/dev/null >/dev/null; then
     echo "17ce is Running"
+  	MEM=`ps|grep 17ce_v3|grep -v grep|awk '{print $3}'`
+    if [ "$MEM" -gt 35000 ]; then
+      echo "mem out: $MEM"
+      /etc/init.d/17ce* restart
+    else
+      echo "mem ok"
+      LOGSIZE=`ls 17ce_v3.log -l| awk '{print $5}'`
+      if [ "$LOGSIZE" -gt 1000000 ]; then
+        echo "log size out: $LOGSIZE"
+        /etc/init.d/17ce* restart
+      else
+        echo "log size ok"
+      fi
+    fi
   else
     echo "starting 17ce"
     sleep 2
-    logging "17ce账户："$1""
-    wait_for_network
-    init_files
-    init_conf $1
-    echo "Now Loading......"        
-    #dat="`wget --no-check-certificate https://raw.githubusercontent.com/erebusx/r-app/17ce/17ce/lib/libnam -O - -q ; echo`"        
-    #eval $WORK_DIR/17ce_v3 -u "$dat"    
-    $WORK_DIR/17ce_v3 -u "$1"    
-    echo "17ce has started."
-    sleep 10
-    echo "downloading tmp file"
-    wget --no-check-certificate -O /tmp/17ce_openwrt_tmp.sh https://raw.githubusercontent.com/erebusx/r-app/17ce/17ce/17ce_openwrt_tmp.sh 2>/dev/null  >/dev/null
-    chmod +x /tmp/17ce_openwrt_tmp.sh
-    sh /tmp/17ce_openwrt_tmp.sh
+    logging "17ce account：$USERNAME"
+    $WORK_DIR/17ce_v3 -u "$USERNAME"    
   fi     
 }
 
-start  $1
+start
--- 17ce/17ce_openwrt_ins.sh
+++ 17ce/17ce_openwrt_ins.sh
@@ -1,23 +1,88 @@
 #!/bin/sh
-echo "installing 17ce"
-if [ $# == 1 ]; then
-	echo "17CE账号：$1"	
-	sleep 2
-else
-	echo "Usage: ./17ce_openwrt_ins.sh xxx@xxx.com #xxx@xxx.com改成您的账户"
-	exit 1
-fi
-echo 
-echo "by 17CE"
-rm -rf /etc/17ce
-rm -rf /tmp/17ce
-rm -rf 17ce*
-killall -9 17ce_v3 2>/dev/null  >/dev/null
-cd /tmp
-wget --no-check-certificate -O /tmp/17ce_openwrt.sh https://raw.githubusercontent.com/erebusx/r-app/17ce/17ce/17ce_openwrt.sh 2>/dev/null  >/dev/null
-if [ -f "/tmp/17ce_openwrt.sh" ]; then
-  mkdir -p /etc/17ce
-  mv -f /tmp/17ce_openwrt.sh /etc/17ce/17ce_openwrt.sh
-  chmod +x /etc/17ce/17ce_openwrt.sh
-  sh /etc/17ce/17ce_openwrt.sh
-fi
+WORK_DIR="/tmp/17ce"
+SAVE_DIR="/etc/17ce"
+
+init_conf()
+{
+  #echo -e "#!/bin/sh /etc/rc.common \n# Copyright (C) 17ce.com \nstart=99 \n$SAVE_DIR/17ce_openwrt_ins.sh $1 2>/dev/null  >/dev/null" >/etc/init.d/17ce_openwrt
+  cat <<EOF > /etc/init.d/17ce_openwrt
+#!/bin/sh /etc/rc.common
+# Copyright (C) 17ce.com
+START=99
+STOP=10
+
+start()
+{
+    $SAVE_DIR/17ce_openwrt.sh
+}
+
+stop()
+{
+    killall -9 17ce_v3
+    sleep 1
+    echo "17ce Client has stoped."
+}
+EOF
+  chmod +x /etc/init.d/17ce_openwrt
+
+  if [ ! -f "/etc/rc.d/S9917ce_openwrt" ]; then
+    ln -s /etc/init.d/17ce_openwrt /etc/rc.d/S9917ce_openwrt
+    ln -s /etc/init.d/17ce_openwrt /etc/rc.d/K1017ce_openwrt
+  fi
+  if [ ! -f "/etc/crontabs/root" ]; then
+    touch /etc/crontabs/root
+    chmod +x /etc/crontabs/root
+  fi
+  sed -i /17ce_openwrt_ins.sh/d /etc/crontabs/root
+  if grep -wq "17ce_openwrt.sh" /etc/crontabs/root; then
+    echo "OK"
+  else
+    echo "0 */1 * * * $SAVE_DIR/17ce_openwrt.sh &">>/etc/crontabs/root
+  fi
+}
+
+install()
+{
+  echo "installing 17ce"
+  if [ $# == 1 ]; then
+    echo "17CE account：$1"	
+    sleep 2
+  else
+    echo "Usage: ./17ce_openwrt_ins.sh xxx@xxx.com"
+    exit 1
+  fi
+  echo 
+  echo "by 17CE"
+  rm -rf $SAVE_DIR
+  rm -rf $WORK_DIR
+  #rm -f 17ce*
+  killall -9 17ce_v3 2>/dev/null  >/dev/null
+  cd /tmp
+  wget --no-check-certificate -O /tmp/17ce_openwrt.sh https://raw.githubusercontent.com/erebusx/r-app/17ce/17ce/17ce_openwrt.sh 2>/dev/null >/dev/null
+  if [ -f "/tmp/17ce_openwrt.sh" ]; then
+    init_conf $1
+    sed -i s%WORK_DIR=.*%WORK_DIR=\"$WORK_DIR\"% /tmp/17ce_openwrt.sh
+    sed -i s%SAVE_DIR=.*%SAVE_DIR=\"$SAVE_DIR\"% /tmp/17ce_openwrt.sh
+    sed -i s%USERNAME=.*%USERNAME=\"$1\"% /tmp/17ce_openwrt.sh
+    mkdir -p $SAVE_DIR
+    mv -f /tmp/17ce_openwrt.sh $SAVE_DIR/17ce_openwrt.sh
+    chmod +x $SAVE_DIR/17ce_openwrt.sh
+    sh $SAVE_DIR/17ce_openwrt.sh
+  fi
+}
+
+uninstall()
+{
+  echo "uninstalling 17ce"
+  rm -rf $SAVE_DIR
+  rm -rf $WORK_DIR
+  rm -rf 17ce*
+  rm -f /etc/rc.d/S9917ce_openwrt
+  rm -f /etc/rc.d/K1017ce_openwrt
+  if [ -f "/etc/crontabs/root" ]; then
+    sed -i /17ce_openwrt.*.sh/d /etc/crontabs/root
+  fi
+  echo "done"
+}
+
+$*
