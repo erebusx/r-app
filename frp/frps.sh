ab09f5df93ee7ca442d079b5b3a0b4d619c9fe3d
xch <@>
2018-11-13 22:28:29 +0800

    frps

---
+++ frp/frps.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+WORK_DIR="/tmp/frp"
+SAVE_DIR="/etc/frp"
+EXEC_BIN="$WORK_DIR/frps"
+export LD_LIBRARY_PATH=/lib:$WORK_DIR
+logging()
+{
+  echo $1
+  logger -t "【frps】" "$1"
+}
+
+check_exec()
+{
+  if [ ! -f "$EXEC_BIN" ]; then
+    logging "$EXEC_BIN not found"
+    killall -9 frps 2>/dev/null  >/dev/null
+    rm -rf $WORK_DIR
+    mkdir -p $WORK_DIR
+	cd $WORK_DIR
+	tar zxvf $SAVE_DIR/frps.tar.gz
+	cp $SAVE_DIR/myfrps.ini .
+  fi
+}
+
+start()
+{
+  check_exec
+  cd $WORK_DIR
+  if ps|grep -w "frps -c"|grep -v grep 2>/dev/null >/dev/null; then
+    logging "frps is Running"
+  else
+    logging "starting frps"
+    sleep 2
+    ./frps -c ./myfrps.ini &
+  fi     
+}
+
+stop()
+{
+  logging "stopping frps"
+  sleep 2
+  killall -9 frps
+}
+
+restart()
+{
+  logging "restarting frps"
+  sleep 2
+  stop
+  sleep 2
+  start
+}
+
+$*
---
+++ frp/frps_ins.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+WORK_DIR="/tmp/frp"
+SAVE_DIR="/etc/frp"
+
+init_conf()
+{
+  #echo -e "#!/bin/sh /etc/rc.common \n# Copyright (C) 17ce.com \nstart=99 \n$SAVE_DIR/17ce_openwrt_ins.sh $1 2>/dev/null  >/dev/null" >/etc/init.d/17ce_openwrt
+  cat <<EOF > /etc/init.d/frps
+#!/bin/sh /etc/rc.common
+START=99
+STOP=10
+
+start() {
+    $SAVE_DIR/frps.sh start
+}
+
+restart() {
+    $SAVE_DIR/frps.sh restart
+}
+
+stop() {
+    $SAVE_DIR/frps.sh stop
+}
+EOF
+  chmod +x /etc/init.d/frps
+  /etc/init.d/frps enable
+
+  if [ ! -f "/etc/rc.d/S99frps" ]; then
+    ln -s /etc/init.d/frps /etc/rc.d/S99frps
+    ln -s /etc/init.d/frps /etc/rc.d/K10frps
+  fi
+  if [ ! -f "/etc/crontabs/root" ]; then
+    touch /etc/crontabs/root
+    chmod +x /etc/crontabs/root
+  fi
+  if grep -wq "frps.sh" /etc/crontabs/root; then
+    echo "OK"
+  else
+    echo "0 */1 * * * $SAVE_DIR/frps.sh start &">>/etc/crontabs/root
+  fi
+}
+
+install()
+{
+  echo "installing frps"
+  rm -rf $SAVE_DIR
+  rm -rf $WORK_DIR
+  killall -9 frps 2>/dev/null  >/dev/null
+  cd /tmp
+  wget -q --no-check-certificate -O /tmp/frps.sh https://raw.githubusercontent.com/erebusx/r-app/master/frp/frps.sh
+  if [ -f "/tmp/frps.sh" ]; then
+    init_conf
+    mkdir -p $SAVE_DIR
+    mv -f /tmp/frps.sh $SAVE_DIR/frps.sh
+    chmod +x $SAVE_DIR/frps.sh
+  fi
+  sleep 2
+  echo "put your config named 'myfrps.ini' into $SAVE_DIR, then run '$SAVE_DIR/frps.sh start'"
+}
+
+uninstall()
+{
+  echo "uninstalling frps"
+  killall -9 frps
+  rm -rf $SAVE_DIR
+  rm -rf $WORK_DIR
+  rm -f /etc/rc.d/S9917ce_openwrt
+  rm -f /etc/rc.d/K1017ce_openwrt
+  if [ -f "/etc/crontabs/root" ]; then
+    sed -i /frps.sh/d /etc/crontabs/root
+  fi
+  echo "done"
+}
+
+$*
